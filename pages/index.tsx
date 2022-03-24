7731621f3995b7dcccb0703210b78b8fcc45fb2e
Jimmy Wang <jwwang.2003@gmail.com>
2022-03-24 03:18:14 -0700

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,20 @@ import type { NextPage } from 'next'
 
 import Main from '../components/Cards/Home/Main'
 import Contact from '../components/Cards/Home/Contact'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    if(typeof window !== "undefined") {
+      const resourcesStatus = window.performance.getEntriesByType('resource')
+      .reduce((formattedOutput, resourceDetails) => formattedOutput.concat({
+          resourceName: resourceDetails.name,
+          cached: resourceDetails.transferSize ? false : true
+        }), [] as object[])
+        console.log(resourcesStatus)
+    }
+  })
+
   return (
     <>
       <Main />
